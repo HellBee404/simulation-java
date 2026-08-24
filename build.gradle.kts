28f8daaafb2803cc4458488125b50b2be856db14
HellBee <sgilsasha1488@gmail.com>
2026-08-24 16:26:12 +0300

    init commit

---
+++ build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    id("java")
+}
+
+group = "by.hellbee"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+}
---
+++ src/main/java/by/hellbee/Main.java
@@ -0,0 +1,7 @@
+package by.hellbee;
+
+public class Main {
+
+    public static void main(String[] args) {
+    }
+}
